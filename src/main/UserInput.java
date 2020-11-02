d3409456123cac22f53cf404eea35296565a82b5
Hiep Do <gao619@gmail.com>
2020-11-02 15:13:24 -0500

    Added the database SQL script

--- src/main/UserInput.java
+++ src/main/UserInput.java
@@ -1,3 +1,5 @@
+package main;
+
 import java.time.*;
 import java.util.Date;
 import java.util.Scanner;
@@ -19,13 +21,12 @@ public class UserInput {
         String gender = getInput.next();
         System.out.print("Please enter your Age: ");
         int age = getInput.nextInt();
-        System.out.print("Please enter your Date(dd/MM/yyyy): ");
+        System.out.print("Please enter your Date(dd-MM-yyyy): ");
         String date = getInput.next();
         System.out.print("Please enter your Departure Time(hh:mm:ss): ");
         String depatureTime = getInput.next();
         System.out.print("Please enter your Destination: ");
         String destination = getInput.next();
-
     }
 
     public static void getDepatureTimeandDate () {
