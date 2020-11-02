fca3e89310da074c93680283356daccb369a2ded
Kyle <kyle.i.dick@outlook.com>
2020-11-02 15:49:44 -0500

    Added Hiep's boarding_pass table to SQL script

--- sql-script.sql
+++ sql-script.sql
@@ -15,3 +15,19 @@ INSERT INTO schedule(destination, departure, price) VALUES
 ("Grand Rapids", "2020-11-06 12:30:00", 10.00), ("San Francisco", "2020-11-06 02:15:00", 310.67),
 ("Chicago", "2020-11-07 15:00:00", 80.99), ("Detroit", "2020-11-07 12:00:00", 30.40), ("Atlanta", "2020-11-07 08:00:00", 200.12),
 ("Grand Rapids", "2020-11-07 12:30:00", 5.00), ("San Francisco", "2020-11-07 02:15:00", 305.67);
+
+CREATE TABLE boarding_pass (
+  id int NOT NULL AUTO_INCREMENT,
+  name varchar(45) DEFAULT NULL,
+  date date DEFAULT NULL,
+  origin varchar(45) DEFAULT NULL,
+  destination varchar(45) DEFAULT NULL,
+  eta varchar(45) DEFAULT NULL,
+  departure time DEFAULT NULL,
+  email varchar(45) DEFAULT NULL,
+  phone varchar(45) DEFAULT NULL,
+  gender varchar(45) DEFAULT NULL,
+  age int DEFAULT NULL,
+  ticket_price float DEFAULT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
