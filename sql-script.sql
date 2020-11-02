ae9dcd8e52ea8d65b935c439044da00aea710a80
Kyle <kyle.i.dick@outlook.com>
2020-11-02 15:50:26 -0500

    Changed boarding_pass definition

--- sql-script.sql
+++ sql-script.sql
@@ -16,7 +16,7 @@ INSERT INTO schedule(destination, departure, price) VALUES
 ("Chicago", "2020-11-07 15:00:00", 80.99), ("Detroit", "2020-11-07 12:00:00", 30.40), ("Atlanta", "2020-11-07 08:00:00", 200.12),
 ("Grand Rapids", "2020-11-07 12:30:00", 5.00), ("San Francisco", "2020-11-07 02:15:00", 305.67);
 
-CREATE TABLE boarding_pass (
+CREATE TABLE IF NOT EXISTS boarding_pass (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(45) DEFAULT NULL,
   date date DEFAULT NULL,
