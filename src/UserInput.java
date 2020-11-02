59a006c4ba3c683074fc658d8336bf9a58e1306a
Herman Breckenridge <hbrecken@pnw.edu>
2020-11-02 14:59:50 -0600

    Date Time User Input

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,30 +1,59 @@
+import entity.BoardingPassTrain;
+
+import java.text.ParseException;
 import java.time.*;
 import java.util.Date;
 import java.util.Scanner;
+import java.text.SimpleDateFormat;
 
 public class UserInput {
     public static void main(String[] args) {
 
         Scanner getInput = new Scanner(System.in);
-        System.out.println("\n" + "+----------------------------------+");
-        System.out.println("+Welcome to the World Fastest Train+");
-        System.out.println("+----------------------------------+\n");
-        System.out.print("Please enter your Name: ");
-        String name = getInput.next();
-        System.out.print("Please enter your Email: ");
-        String email = getInput.next();
-        System.out.print("Please enter your Phone Number: ");
-        String phoneNumber = getInput.next();
-        System.out.print("Please enter your Gender: ");
-        String gender = getInput.next();
-        System.out.print("Please enter your Age: ");
-        int age = getInput.nextInt();
-        System.out.print("Please enter your Date(dd/MM/yyyy): ");
+        BoardingPassTrain pass1 = new BoardingPassTrain();
+//        System.out.println("\n" + "+----------------------------------+");
+//        System.out.println("+Welcome to the World Fastest Train+");
+//        System.out.println("+----------------------------------+\n");
+//        System.out.print("Please enter your Name: ");
+//        String name = getInput.next();
+//        System.out.print("Please enter your Email: ");
+//        String email = getInput.next();
+//        System.out.print("Please enter your Phone Number: ");
+//        String phoneNumber = getInput.next();
+//        System.out.print("Please enter your Gender: ");
+//        String gender = getInput.next();
+//        System.out.print("Please enter your Age: ");
+//        int age = getInput.nextInt();
+
+        //Date User Input
+        System.out.print("Please enter your Date(dd-MM-yyyy hh:mm:ss): ");
         String date = getInput.next();
-        System.out.print("Please enter your Departure Time(hh:mm:ss): ");
-        String depatureTime = getInput.next();
-        System.out.print("Please enter your Destination: ");
-        String destination = getInput.next();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss");
+        Date date2=null;
+        try {
+            //Parsing the String
+            date2 = dateFormat.parse(date);
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        System.out.println(date2);
+
+//        Time User Input
+//        System.out.print("Please enter your Departure time: ");
+//        String depatureTime = getInput.next();
+//        SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm:ss");
+//        Date time1=null;
+//        try {
+//            //Parsing the String
+//            time1 = timeFormat.parse(depatureTime);
+//        } catch (ParseException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        System.out.println(time1);
+       // String depatureTime = getInput.next();
+//        String destination = getInput.next();
 
     }
 
